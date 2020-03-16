5c51757b2236ce2d8f157992425612f11d6cc5ac
Idonoghue <isaacdonoghue@live.com>
2020-03-16 04:57:19 +0000

    Added default scene; initial window size adjustment, not dynamic yet

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import { Engine, Scene } from 'react-babylonjs';
+import { Vector3 } from 'babylonjs';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Engine
+      antialias={true}
+      //   adaptToDeviceRatio={true}
+      canvasId="sample-canvas"
+      width={window.innerWidth}
+      height={window.innerHeight}
+    >
+      <Scene>
+        <freeCamera
+          name="camera1"
+          position={new Vector3(0, 5, -10)}
+          target={Vector3.Zero()}
+        />
+        <hemisphericLight
+          name="light1"
+          intensity={0.7}
+          direction={Vector3.Up()}
+        />
+        <sphere
+          name="sphere1"
+          diameter={2}
+          segments={16}
+          position={new Vector3(0, 1, 0)}
+        />
+        <ground name="ground1" width={6} height={6} subdivisions={2} />
+      </Scene>
+    </Engine>
   );
 }
 
